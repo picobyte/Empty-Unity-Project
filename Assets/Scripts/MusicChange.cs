1d036a4ad94cc0f4e05e4384b57007e3a8b2fd6c
MacBook-Benutzer <macbook@rzvpn105.informatik.uni-hamburg.de>
2013-10-26 22:20:22 +0200

    added MusicChanger placeholder

---
+++ Assets/Scripts/MusicChange.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicChange : Activatable
+{
+    [RequireComponent (typeof (AudioSource))]
+	void OnTriggerEnter(Collider other) 
+    {
+        if(isActive)
+        {
+           //TODO: play the right music file for each colour.
+        }
+	}
+}
