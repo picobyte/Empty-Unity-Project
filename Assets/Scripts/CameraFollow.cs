b7141520819b71d8944982fed42bfec49dbd3624
MacBook-Benutzer <macbook@rzvpn101.informatik.uni-hamburg.de>
2013-10-26 03:23:13 +0200

    added CameraFollow Script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+public Vector3 offset;
+public float speed = 10;
+
+    public Transform target;    
+    // Update is called once per frame
+    void LateUpdate () {
+        transform.position = Vector3.Lerp(transform.position, offset + target.position, Time.deltaTime*speed);
+    }
+}
