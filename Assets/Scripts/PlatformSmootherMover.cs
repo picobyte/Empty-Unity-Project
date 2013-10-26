cfd20328998a71cd01df0d0cbd62a90e524359b0
MacBook-Benutzer <macbook@rzvpn62.informatik.uni-hamburg.de>
2013-10-26 14:04:26 +0200

    Added PlatformSmootherMover and solidPlatform scripts

---
+++ Assets/Scripts/PlatformSmootherMover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSmootherMover : Activatable {
+
+    //public float blockspeed = 7;
+    //public float waitTime = 2;
+    public Vector3 endPos;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+
+	public bool isMoving { private get; set; }
+	
+	// Use this for initialization
+	void Awake () 
+	{
+		isMoving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var step = Time.deltaTime*blockspeed;
+        if(isActive && isMoving)
+        {
+        transform.position = Vector3.SmoothDamp(transform.position, endPos, ref velocity, smoothTime);
+        }
+	}
+    
+    protected override void OnStatusChange(bool active)
+    {
+    
+    }
+}
---
+++ Assets/Scripts/SolidPlatform.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolidPlatform : Activatable 
+{
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if(isActive)
+        {
+        
+        }
+    }
+    
+    protected override void OnStatusChange(bool active)
+    {
+        collider.isTrigger = !active;
+    }
+}
