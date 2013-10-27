5852404899a91a8b8167ea1954327bc093d91d41
Sascha Graeff <sascha@blackicegames.de>
2013-10-27 12:53:48 +0100

    Changed Background Rendering method.

---
+++ Assets/Scripts/UVAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class UVAnimation : MonoBehaviour
+{
+	public Vector2 speed;
+    
+    private Material mat;
+    private Vector2 pos;
+    
+    
+    void Awake()
+    {
+        mat = renderer.material;
+        pos = mat.mainTextureOffset;
+    }
+    
+	void Update()
+    {
+        pos += speed * Time.deltaTime;
+        
+        pos.x = Mathf.Repeat(pos.x, 1);
+        pos.y = Mathf.Repeat(pos.y, 1);
+    
+        mat.mainTextureOffset = pos;
+	}
+}
