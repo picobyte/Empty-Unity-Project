475b944d53ee0c8e7bfc99675b22dd5c69b23069
Damian Hofmann <picobyte@gmx.com>
2013-10-27 21:11:09 +0100

    Fix red platforms!

--- Assets/Scripts/PlatformActivateTrigger.cs
+++ Assets/Scripts/PlatformActivateTrigger.cs
@@ -1,66 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlatformActivateTrigger : MonoBehaviour 
-{
-	public GameObject target;
-	public bool holdPlayer = true;
-    
-    private Transform player;
-	
-	void OnTriggerEnter(Collider other) 
-	{
-        if(!other.CompareTag("Player")) return;
-    
-        StopAllCoroutines();
-    
-		var mover = target.GetComponent<PlatformMover>();
-		if(mover && mover.isActive)
-		{
-Debug.Log("Ghost entered lift platform.");
-			mover.playerOnIt = true;
-			if(holdPlayer)
-            {
-				player = other.transform;
-			}
-		}
-	}
-	
-	void OnTriggerExit(Collider other) 
-	{
-        if(!other.CompareTag("Player")) return;
-    
-Debug.Log("Ghost left lift platform.");
-		var mover = target.GetComponent<PlatformMover>();
-		if(holdPlayer)
-        {
-			if(mover)
-            {
-                StartCoroutine(DetachPlayer());
-			}
-		}
-	}
-    
-    IEnumerator DetachPlayer()
-    {
-print("start detaching");
-        yield return new WaitForFixedUpdate();
-        yield return new WaitForFixedUpdate();
-        if(player)
-        {
-print("detaching");
-            var mover = target.GetComponent<PlatformMover>();
-            mover.playerOnIt = false;
-            player = null;
-        }
-    }
-    
-    private Vector3 previousPosition;
-    void LateUpdate()
-    {
-if(player) print("lateupdate");
-        if(player) player.Translate(transform.position - previousPosition, Space.World);
+public class PlatformActivateTrigger : MonoBehaviour
+{;
+        public GameObject target;
+        public bool holdPlayer = true;
         
-        previousPosition = transform.position;
-    }
+        void OnTriggerEnter(Collider other)
+        {
+                var mover = target.GetComponent<PlatformMover>();
+                if(mover && mover.isActive && !mover.playerOnIt)
+                {
+                        Debug.Log("Ghost entered lift platform.");
+                        mover.playerOnIt = true;
+                        if(holdPlayer) {
+								var player =
+									other.gameObject.GetComponent<PlayerMovement>();
+								if(player) {
+									other.transform.parent = target.transform;
+								}
+                        }
+                }
+        }
+        
+        void OnTriggerExit(Collider other)
+        {
+                Debug.Log("Ghost left lift platform.");
+                var mover = target.GetComponent<PlatformMover>();
+                if(holdPlayer) {
+					if(mover) {
+							mover.playerOnIt = false;
+					}
+					var player = other.gameObject.GetComponent<PlayerMovement>();
+					if(player) {
+						other.transform.parent = null;
+					}
+                }
+        }
 }
