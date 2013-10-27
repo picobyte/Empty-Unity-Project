0495f6e83e0aec5c1de52dbf5b0082376e25be55
Sascha Graeff <sascha@blackicegames.de>
2013-10-27 14:32:41 +0100

    Added Random Start Rotation ro SimpleRotate Script.

--- Assets/Scripts/SimpleRotate.cs
+++ Assets/Scripts/SimpleRotate.cs
@@ -5,6 +5,11 @@ public class SimpleRotate : MonoBehaviour
 {
 	public Vector3 rotation;
 	
+    void Start()
+    {
+        transform.Rotate(rotation * Random.value * 360);
+    }
+    
 	void Update()
     {
         transform.Rotate(rotation * 10 * Time.deltaTime);
